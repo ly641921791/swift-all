cdecee6bc344695a289571c2dae3641a80df8dd8
Ly641921791 <641921791@qq.com>
2019-03-21 15:40:39 +0800

    Add ColumnFire tableName

--- swift-core/src/main/java/com/github/ly641921791/swift/core/mapper/method/FindOneByColumn.java
+++ swift-core/src/main/java/com/github/ly641921791/swift/core/mapper/method/FindOneByColumn.java
@@ -1,16 +1,20 @@
 package com.github.ly641921791.swift.core.mapper.method;
 
+import com.github.ly641921791.swift.core.mapper.AbstractSelectMethodResolver;
 import com.github.ly641921791.swift.core.metadata.Table;
+import com.github.ly641921791.swift.jdbc.SqlScript;
 import com.github.ly641921791.swift.session.SwiftConfiguration;
 
 /**
  * @author ly
  * @since 1.0.0
  **/
-public class FindOneByColumn extends FindAllByColumn {
+public class FindOneByColumn extends AbstractSelectMethodResolver {
 
     @Override
-    public String buildSqlScript(Table table, SwiftConfiguration configuration) {
-        return super.buildSqlScript(table, configuration).replace(TAG_SCRIPT_CLOSE, "LIMIT 1" + TAG_SCRIPT_CLOSE);
+    protected void handlerWhere(SqlScript sqlScript, Table table, SwiftConfiguration configuration) {
+        sqlScript.WHERE("`${c}` = #{v}");
+        handlerDeleteColumn(sqlScript, table, configuration);
     }
+
 }
