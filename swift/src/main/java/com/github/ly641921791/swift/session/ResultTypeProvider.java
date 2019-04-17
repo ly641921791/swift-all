8677b023262af7929f8101c20b330e8154b6dae2
Ly641921791 <641921791@qq.com>
2019-04-17 18:54:36 +0800

    [Opt] 提交部分文件，准备通过参数控制返回值类型

---
+++ swift/src/main/java/com/github/ly641921791/swift/session/ResultTypeProvider.java
@@ -0,0 +1,12 @@
+package com.github.ly641921791.swift.session;
+
+/**
+ * @author ly
+ * @since 1.0.0
+ **/
+@FunctionalInterface
+public interface ResultTypeProvider {
+
+    Class<?> provide();
+
+}
